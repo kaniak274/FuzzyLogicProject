42f6e314fdc352854ce84174e47ccef25e2f5b30
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-05-09 21:02:35 +0200

    Add some strings for absolute quantificator

--- src/quantifiers/AbsoluteQ.java
+++ src/quantifiers/AbsoluteQ.java
@@ -1,10 +1,12 @@
 package quantifiers;
 
+import java.util.Random;
+
 import fuzzy_set.FuzzySet;
 
 public class AbsoluteQ {
     public static String exactMatching(FuzzySet set, Matcher matcher) {
-        return Long.toString(countMatchingElements(set, matcher));
+        return getString(countMatchingElements(set, matcher));
     }
 	
     private static long countMatchingElements(FuzzySet set, Matcher matcher) {
@@ -12,5 +14,17 @@ public class AbsoluteQ {
             .getStreamOfSet()
             .filter(element -> matcher.matcher(element.getMembership()))
             .count();
+    }
+    
+    private static String getString(long count) {
+        String[] terms = {"Oko³o ", "Ponad ", "Wiêcej ni¿ "};
+        Random generator = new Random();
+        int num = generator.nextInt(3);
+    	
+        if (count <= 1) {
+            return terms[0] + count;
+        }
+        
+    	return terms[num] + (count - 1);
     }
 }
