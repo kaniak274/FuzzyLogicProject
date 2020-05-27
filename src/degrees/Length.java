0d42813ec2f96d15330a0a1eb49feb9f30c20b07
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-05-27 19:50:05 +0200

    Add T5 degree

---
+++ src/degrees/Length.java
@@ -0,0 +1,11 @@
+package degrees;
+
+import java.util.List;
+
+import terms.TermData;
+
+public class Length {
+    public static double calculate(List<TermData> sets) {
+        return sets.size();
+    }
+}
